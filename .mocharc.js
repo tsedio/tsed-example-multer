030c8188e471aabf08c27f7faa64b4ab89cc5b56
Travis CI <travis@travis-ci.org>
2020-03-16 09:39:46 +0000

    'Deploy project with Ts.ED v5.44.8'

--- .mocharc.js
+++ .mocharc.js
@@ -1,12 +1,13 @@
+
 module.exports = {
   require: [
-    'ts-node/register/transpile-only',
-    'tsconfig-paths/register',
-    'scripts/mocha/register'
+    "ts-node/register/transpile-only",
+    "tsconfig-paths/register",
+    "scripts/mocha/register"
   ],
   recursive: true,
-  reporter: 'dot',
+  reporter: "dot",
   spec: [
-    '**/*.spec.ts'
+    "**/*.spec.ts"
   ]
-}
+};
